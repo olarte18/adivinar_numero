9d2945df97205be90c8e4a89c3259d5b42bdeb3c
olarte18 <snheider2224656@correo.uis.edu.co>
2022-10-05 15:32:51 -0500

    codigo y correccion diagrama

--- adivinar_numrto.py
+++ adivinar_numrto.py
@@ -1,1 +1,23 @@
 #input 
+import random
+print("---------------------")
+print("--ADIVINA EL NUMERO--")
+print("---------------------")
+
+g=random.randint(0,1000)
+n=int(input("Digite un numero entre un numero 0 y 1000: "))
+
+if n>=0 and n<=1000:
+    while n!=g:
+    
+        if n>g:
+            print("INCORRECTO: Intente con un numero menor")
+        else:
+            print("INCORRECTO: Intente con un numero mayor")
+
+        n=int(input("Digite un numero entre un numero 0 y 1000: "))
+    print ("el numero es:"+str(n))
+    
+else: 
+    print ("NO CUMPLE CONDICIONES")
+
