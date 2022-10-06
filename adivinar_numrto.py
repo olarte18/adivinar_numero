d272e7a24e6e5cff3fc3eeddcd4edf5dcbee3635
olarte18 <snheider2224656@correo.uis.edu.co>
2022-10-06 10:05:56 -0500

    conteo

--- adivinar_numrto.py
+++ adivinar_numrto.py
@@ -6,17 +6,17 @@ print("---------------------")
 
 g=random.randint(0,1000)
 n=int(input("Digite un numero entre un numero 0 y 1000: "))
-
+i=0
 if n>=0 and n<=1000:
     while n!=g:
+        i=i+1
     
         if n>g:
             print("INCORRECTO: Intente con un numero menor")
         else:
             print("INCORRECTO: Intente con un numero mayor")
-
         n=int(input("Digite un numero entre un numero 0 y 1000: "))
-    print ("el numero es:"+str(n))
+    print ("el numero es:"+str(n)+", lo logro en el intento "+ str(i))
     
 else: 
     print ("NO CUMPLE CONDICIONES")
